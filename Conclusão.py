240a0cb46f28d14e8efbf04b379ac5fca10b04d6
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-10-27 16:17:23 -0300

    Update Conclusão.py

--- Conclusão.py
+++ Conclusão.py
@@ -5,7 +5,25 @@ def imc():
     n2 = float(inputt_n2.get())
     
     resultado = n1 / (n2**2)
-    label_resultado['text']= f'resultado é {resultado}'
+    bah= round(resultado, 1)
+    label_resultado['text']= f'resultado é {bah}'
+
+    if resultado >=18.6 and resultado <= 24.9:
+        label_imc['text']= 'Você está Normal'
+    elif resultado >= 25 and resultado <= 29:
+        label_imc['text']= 'Você está em Sobrepeso'
+    elif resultado >= 30 and resultado <= 34.9:
+        label_imc['text']= 'Você está em Obesidade I'
+    elif resultado >= 35 and resultado <= 39.9:
+        label_imc['text']= 'Você está em Obesidade II'
+    elif resultado > 40:
+        label_imc['text']= 'Você está em Obesidade III'
+    elif resultado < 18.6:
+        label_imc['text']= 'Você está Abaixo do peso'
+        
+
+
+
 
 root = tk.Tk()
 root.title('Calculo IMC')
@@ -29,8 +47,15 @@ btn_calcular.grid (row=4, column=1)
 label_resultado = tk.Label (root, text = 'Resultado: ')
 label_resultado.grid(row=5, column=0)
 
-Calculo= tk.Label(root, text= f'O calculo é peso / (altura*altura)')
+Calculo= tk.Label(root, text= f'O calculo é: peso / (altura*altura)')
 Calculo.grid (row=6, column=0)
+
+label_imc = tk.Label(root, text= 'Você está em ')
+label_imc.grid(row=5, column=1)
+
+
+
+
 #packs
 
 root.mainloop()
